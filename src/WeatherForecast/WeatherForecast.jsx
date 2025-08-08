28be39b45e647df92a28d8223f8bcbd9635fc9ef
Shaina G <shainagtherapy@gmail.com>
2025-08-07 20:38:42 -0700

    Lab complete!

---
+++ src/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,15 @@
+import './WeatherForecast.css';
+
+const WeatherForecast = (props) => {
+    const { day, img, imgAlt, conditions, time } = props;
+    return ( 
+        <div className="weather">
+            <h2> {day} </h2>
+            <img src={ img } alt={imgAlt} />
+            <p><span>conditions: </span>{conditions}</p>
+            <p><span>time: </span>{time}</p>
+        </div>
+    )
+}
+
+export default WeatherForecast;
